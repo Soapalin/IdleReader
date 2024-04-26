63d11d80858c47385777e26f1d8a5bc7a1b7f791
Lucien Tran <46849667+Soapalin@users.noreply.github.com>
2024-04-26 14:46:16 +1000

    added debug file to documents and set env auto

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-
+	os.Setenv("DEBUG", "true")
 	if len(os.Getenv("DEBUG")) > 0 {
-		f, err := tea.LogToFile("debug.log", "[DEBUG]")
+		dir, err := os.UserHomeDir()
+		if err != nil {
+			panic(err)
+		}
+		debugFile := filepath.Join(dir, "Documents", "debug.log")
+		f, err := tea.LogToFile(debugFile, "[DEBUG]")
 		if err != nil {
 			fmt.Println("fatal:", err)
 			os.Exit(1)
