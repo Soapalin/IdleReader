5995c90e3400e548aef17b71ef95b247a38762dd
Lucien Tran <46849667+Soapalin@users.noreply.github.com>
2024-05-06 14:43:39 +1000

    Add files via upload

--- theme/theme.go
+++ theme/theme.go
@@ -23,3 +23,11 @@ var InputStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF06B7"))
 var Pink = lipgloss.Color("5")
 
 var CursorArrow = lipgloss.NewStyle().Foreground(Pink).Blink(true).Bold(true).Render(" ←")
+
+var HelpIcon = lipgloss.NewStyle().Foreground(lipgloss.Color("#7B7B7B"))
+var HelpText = lipgloss.NewStyle().Foreground(lipgloss.Color("#424242"))
+
+var SpinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+
+var ActiveDotPaginator = lipgloss.NewStyle().Foreground(Pink).Render("•")
+var InactiveDotPaginator = lipgloss.NewStyle().Render("•")
