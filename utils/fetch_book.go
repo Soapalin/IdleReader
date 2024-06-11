e394138bb177699838949fc18875826a9dcd0e97
Soapalin <lucien.a.tran@gmail.com>
2024-06-11 20:24:13 +1000

    added fetchbook api

---
+++ utils/fetch_book.go
@@ -0,0 +1,8 @@
+package utils
+
+
+
+var steps []string = {
+	"https://openlibrary.org/subjects/fantasy.json?published_in=2015-2024?limits=100", // get books by genre
+	"https://openlibrary.org/works/OL138052W/editions.json" //get page number here and average 
+}
